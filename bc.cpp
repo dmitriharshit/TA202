ff53eed9e3dea19ccffa41b4ca5580846ba91785
Harshit Shakya <harshitshakya1@gmail.com>
2023-02-12 00:29:21 +0530

    added bc

---
+++ bc.cpp
@@ -0,0 +1,21 @@
+ Volume in drive D is DATA
+ Volume Serial Number is 74A9-7CAF
+
+ Directory of D:\Desktop\TA_Project
+
+12-02-2023  00:22    <DIR>          .
+12-02-2023  00:15    <DIR>          ..
+05-02-2023  22:56         2,163,749 assembly.f3z
+05-02-2023  14:47           805,376 Assembly1.iam
+12-02-2023  00:22                 0 bc.cpp
+09-02-2023  22:25           490,498 Gear (60mm) v1.f3d
+10-02-2023  00:49           148,035 Large_gear.pdf
+09-02-2023  22:25            45,995 L_Support v1.f3d
+10-02-2023  00:25            92,929 L_support.pdf
+09-02-2023  22:25         1,104,421 Small Gear(30mm) v1.f3d
+10-02-2023  00:32           125,403 Small_gear'.pdf
+07-02-2023  22:02           187,824 ta202.f3d
+12-02-2023  00:21               856 test.txt
+09-02-2023  22:25           128,201 Wheel_ v2.f3z
+              12 File(s)      5,293,287 bytes
+               2 Dir(s)  877,770,231,808 bytes free
